306f3db26becd7ae10a75432105f8355fb7f37fc
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-12-17 10:09:46 -0300

    Configurando Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -15,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={inter.variable} 
-      >
+    <html className={inter.variable} lang="pt">
+      <body>
         {children}
       </body>
     </html>
