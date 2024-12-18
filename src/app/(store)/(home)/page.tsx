edbdd7174260cd05ed4bb10d9dfc221a5202052d
Matheus Maschio <matheuslmaschi0@hotmail.com>
2024-12-18 08:37:51 -0300

    Page Metadata no Next.js (SEO)

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,11 +1,8 @@
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
-
-/**
- * Cache & Memoization
- */
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api('/products/featured', {
@@ -17,6 +14,10 @@ async function getFeaturedProducts(): Promise<Product[]> {
   const products = await response.json()
 
   return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
 }
 
 export default async function Home() {
